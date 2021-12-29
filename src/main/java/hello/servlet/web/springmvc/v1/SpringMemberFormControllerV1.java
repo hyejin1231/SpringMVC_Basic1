345ccf39a01cd9d8bd00a807e68bf5319a599feb
hyejin1231 <phj981231@naver.com>
2021-12-29 10:33:14 +0900

    20211229 inflearn Spring MVC #1 Spring MVC 시작 - Controller 분할 버전

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,14 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller // 안에 @Component가 있기 때문에 자동으로 스프링 빈에 등록된다.
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
